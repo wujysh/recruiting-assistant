12d2aa4dd8441541a8729445075dbc8c600e5169
Jiaye Wu <wujysh@gmail.com>
2018-04-18 02:53:49 +0800

    Clean up sidebar

--- src/main/java/com/sap/recruiting/assistant/controller/AdminMainController.java
+++ src/main/java/com/sap/recruiting/assistant/controller/AdminMainController.java
@@ -1,12 +1,8 @@
 package com.sap.recruiting.assistant.controller;
 
-import com.sap.recruiting.assistant.entity.User;
-import com.sap.recruiting.assistant.exception.ServiceException;
-import com.sap.recruiting.assistant.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by Jiaye Wu on 18-3-26.
@@ -14,18 +10,13 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class AdminMainController {
 
-    private final UserService userService;
-
     @Autowired
-    public AdminMainController(UserService userService) {
-        this.userService = userService;
+    public AdminMainController() {
+
     }
 
     @RequestMapping("/admin")
-    public ModelAndView index() throws ServiceException {
-        ModelAndView mav = new ModelAndView("admin/company/list");
-        User currentUser = userService.getSessionUser();
-        mav.addObject("currentUser", currentUser);
-        return mav;
+    public String index() {
+        return "redirect:/admin/company";
     }
 }
